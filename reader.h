e12801a7a88c10d89d05835289ffaf4e0d290f68
Usman Zaheer <55992433+uz1pk@users.noreply.github.com>
2022-07-25 16:06:00 -0400

    Update reader.h

--- reader.h
+++ reader.h
@@ -1,15 +1,8 @@
-/*
-Name: Usman Zaheer
-Class: CIS 2520 (Dr. Stefan Kremer)
-Helper function for reader.c
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
     
-#include "hash.h" //given helper library for hash tables
-
+#include "hash.h"
 
 int entries(char *filebase);
 void createSet(char *code, int until, char *setData, int setDataIndex, char *setDataFile);
